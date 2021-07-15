c83294d5f1774622321ed71862efd2f4439b753e
Imran Ali K <ikhwaja.ext@wabtec.com>
2021-07-15 20:38:51 +0530

    updated the file v1.0

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,3 +1,15 @@
+# Configuring the aws provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.49.0"
+    }
+  }
+}
+
+
 # creating an aws_ec2 iam_instance_profile
 
 resource "aws_instance" "demo-ec2" {
@@ -5,6 +17,6 @@ resource "aws_instance" "demo-ec2" {
   instance_type = "t2.micro"
   
   tags = {
-    Name = "newec2"
+    Name = "newecc2."
   }
 }
